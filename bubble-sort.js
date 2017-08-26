d9f49054b2886cbbe8a754106044830ef0324883
sbernheim <sambernheim@gmail.com>
2017-08-26 13:45:48 -0400

    Add bubble sort algorithm

---
+++ bubble-sort.js
@@ -0,0 +1,17 @@
+module.exports = {
+
+	function bubbleSort(arr) {
+		for (let i = 0; i < arr.length; i++) {
+			for (let j = 0; j < i; j++) {
+
+				if (arr[j] > arr[j+1]) {
+					let temp = arr[j]
+					arr[j] = arr[j+1];
+					arr[j+1] = temp;
+				}
+			}
+		}
+
+		return arr;
+	}
+}
